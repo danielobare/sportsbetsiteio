df0489ec9b8ca15b3d7a308816f81155b5d96eb2
Daniel Obare <dnobare@gmail.com>
2023-10-13 16:21:22 +0300

    new web changes with new styling. styling works on big screens but does not work on mobile/smaller screens.

--- registration.js
+++ registration.js
@@ -1,65 +1,68 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Select the registration form and elements
-    const registrationForm = document.getElementById("registrationForm");
-    const nameInput = document.getElementById("name");
-    const emailInput = document.getElementById("email");
-    const passwordInput = document.getElementById("password");
-    const confirmPasswordInput = document.getElementById("confirmPassword");
-    const termConCheckbox = document.getElementById("termCon");
+document.addEventListener("DOMContentLoaded", function() {
+    // Get references to form elements
+    const loginForm = document.querySelector(".form.login form");
+    const signupForm = document.getElementById("registrationForm");
 
-    // Handle form submission
-    registrationForm.addEventListener("submit", function (e) {
-        e.preventDefault(); // Prevent the form from actually submitting
+    // Add event listeners to forms
+    if (loginForm) {
+        loginForm.addEventListener("submit", function(e) {
+            e.preventDefault(); // Prevent the form from submitting
 
-        // Retrieve the form data
-        const name = nameInput.value;
-        const email = emailInput.value;
-        const password = passwordInput.value;
-        const confirmPassword = confirmPasswordInput.value;
-        const acceptedTerms = termConCheckbox.checked;
+            // Get user input values
+            const email = loginForm.querySelector("input[type='text']").value;
+            const password = loginForm.querySelector("input[type='password']").value;
 
-        // Perform validation (you can add more validation as needed)
-        if (password !== confirmPassword) {
-            alert("Password and confirm password do not match.");
-            return;
+            // Perform client-side validation
+            if (validateLoginForm(email, password)) {
+                // Handle the login process here (e.g., send data to the server)
+                // You can use AJAX or fetch to send data to the server
+                // Display success or error messages to the user
+                console.log("Login submitted with email: " + email);
+            }
+        });
+    }
+
+    if (signupForm) {
+        signupForm.addEventListener("submit", function(e) {
+            e.preventDefault(); // Prevent the form from submitting
+
+            // Get user input values
+            const name = signupForm.querySelector("#name").value;
+            const email = signupForm.querySelector("#email").value;
+            const password = signupForm.querySelector("#password").value;
+            const confirmPassword = signupForm.querySelector("#confirmPassword").value;
+
+            // Perform client-side validation
+            if (validateSignupForm(name, email, password, confirmPassword)) {
+                // Handle the registration process here (e.g., send data to the server)
+                // You can use AJAX or fetch to send data to the server
+                // Display success or error messages to the user
+                console.log("Registration submitted with email: " + email);
+            }
+        });
+    }
+
+    // Function to validate login form
+    function validateLoginForm(email, password) {
+        // Implement your validation logic here
+        // Return true if the input is valid, otherwise return false
+        if (email && password) {
+            return true;
+        } else {
+            alert("Please fill in all fields.");
+            return false;
         }
+    }
 
-        if (!acceptedTerms) {
-            alert("You must accept the terms and conditions.");
-            return;
+    // Function to validate signup form
+    function validateSignupForm(name, email, password, confirmPassword) {
+        // Implement your validation logic here
+        // Return true if the input is valid, otherwise return false
+        if (name && email && password && confirmPassword === password) {
+            return true;
+        } else {
+            alert("Please fill in all fields and make sure passwords match.");
+            return false;
         }
-
-        // Create a data object to send to the server
-        const formData = {
-            name,
-            email,
-            password,
-        };
-
-        // Send the form data to your server for registration
-        fetch("https://your-server-url.com/register", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                // Handle the response from the server
-                if (data.success) {
-                    // Registration was successful
-                    alert("Registration successful!");
-                    registrationForm.reset(); // Reset the form
-                } else {
-                    // Registration failed, handle error
-                    alert("Registration failed. Please try again.");
-                }
-            })
-            .catch((error) => {
-                // Handle any network or server errors
-                console.error("Error:", error);
-                alert("An error occurred. Please try again later.");
-            });
-    });
+    }
 });
